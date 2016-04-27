0e872bc65fd76ce25b292e2b44dd108111f998f6
wily9434 <wily9434@colorado.edu>
2016-04-27 14:26:51 -0600

    Added files via upload

---
+++ myShop.cpp
@@ -0,0 +1,311 @@
+#include "myShop.h"
+#include <sstream>
+
+
+using namespace std;
+
+MyShop::MyShop()
+{
+    //ctor
+    root=NULL;
+}
+
+///Deletes all the nodes after you quit the game////////////////
+void MyShop::DeleteAll(ShopNode* node)
+{
+    if (node->leftChild!=NULL)
+		DeleteAll(node->leftChild);
+	if (node->rightChild!=NULL)
+		DeleteAll(node->rightChild);
+	///cout<<"Deleting: "<<node->title<<endl;
+	delete node;
+}
+///1/////////////////////////////////////////////////////////
+
+
+
+MyShop::~MyShop()
+{
+    //dtor
+    DeleteAll(root);
+}
+
+
+///ADDs the initial nodes into the tree, we get the items from the text file/////////////////////
+void MyShop::addMovieNode(std::string title, int attribute, int cost, int quantity)
+{
+    ShopNode* tmp=root;
+    ShopNode* parent=NULL;
+    ShopNode* node=new ShopNode(title, attribute, cost, quantity);
+    node->title=title;
+    node->leftChild=NULL;
+    node->rightChild=NULL;
+    while (tmp!=NULL)
+    {
+        parent=tmp;
+        if (node->title.compare(tmp->title)>0)
+            tmp=tmp->rightChild;
+        else if (node->title.compare(tmp->title)<0)
+            tmp=tmp->leftChild;
+    }
+    if (root==NULL)
+        root=node;
+    else if (node->title.compare(parent->title)<0)
+    {
+        parent->leftChild=node;
+        node->parent=parent;
+    }
+    else
+    {
+        parent->rightChild=node;
+        node->parent=parent;
+    }
+}
+///1//////////////////////////////////////////////////////////////
+
+///Adds the player inventory Initialize///////////////////////////////////////////////////////////////////
+void MyShop::addPlayerNode(std::string itemName, int itemAttribute, int itemCost, int itemQuantity)
+{
+    PlayerNode* tmp=playerRoot;
+    PlayerNode* parent=NULL;
+    PlayerNode* node=new PlayerNode(itemName, itemAttribute, itemCost, itemQuantity);
+    node->itemName=itemName;
+    node->leftChild=NULL;
+    node->rightChild=NULL;
+    while (tmp!=NULL)
+    {
+        parent=tmp;
+        if (node->itemName.compare(tmp->itemName)>0)
+            tmp=tmp->rightChild;
+        else if (node->itemName.compare(tmp->itemName)<0)
+            tmp=tmp->leftChild;
+    }
+    if (playerRoot==NULL)
+        playerRoot=node;
+    else if (node->itemName.compare(parent->itemName)<0)
+    {
+        parent->leftChild=node;
+        node->parent=parent;
+    }
+    else
+    {
+        parent->rightChild=node;
+        node->parent=parent;
+    }
+}
+///1/////////////////////////////////////////////////////////////////////////////
+
+
+///Adds the item into the players inventory after buying it from shop/////////////
+void MyShop::addPlayerInventory(std::string itemName, int itemAttribute, int itemCost, int itemQuantity){
+
+        addPlayerNode(itemName, itemAttribute, itemCost, itemQuantity);
+        cout<< "Player has bought " << itemName << " for " << itemCost << " gp" <<endl;
+
+
+
+}
+///1/////////////////////////////////////////////////////////////////////
+
+
+///Sells items back to the shop///////////////////////////////////
+void MyShop::sellItem(std::string title){
+    ShopNode* node=root;
+    bool found;
+	while (node!=NULL)
+	{
+		if (node->title.compare(title)>0)
+			node=node->leftChild;
+		else if (node->title.compare(title)<0)
+			node=node->rightChild;
+		else if (node->title==title)
+        {
+            found=true;
+            break;
+        }
+        else
+        {
+            found=false;
+            break;
+        }
+	}
+
+	if (found==true)
+    {
+        cout<< endl;
+        cout<<node->title << " has been sold!"<<endl;
+        node->quantity++;
+        cout<<"==========="<<endl;
+        cout<<"|Item info|"<<endl;
+        cout<<"==========="<<endl;
+        cout<<"Item: "<<node->title<<endl;
+        cout<<"cost: "<<node->cost<<endl;
+        cout<<"Quantity: "<<node->quantity<<endl;
+        cout<<"==========="<<endl;
+        cout<< endl;
+
+    }
+    else if (found==false){
+        cout<<"Item not found."<<endl;
+        cout<<"Please only type in items that are in the list"<<endl;
+    }
+
+}
+///1////////////////////////////////////////////////////////////////////
+
+
+///Finds a specific item in the shop, giving details about it////////////////
+void MyShop::findMovie(std::string title)
+{
+    ShopNode* node=root;
+    bool found;
+	while (node!=NULL)
+	{
+		if (node->title.compare(title)>0)
+			node=node->leftChild;
+		else if (node->title.compare(title)<0)
+			node=node->rightChild;
+		else if (node->title==title)
+        {
+            found=true;
+            break;
+        }
+        else
+        {
+            found=false;
+            break;
+        }
+	}
+	if (found==true)
+    {
+        cout<< endl;
+        cout<<"==========="<<endl;
+        cout<<"Item Info"<<endl;
+        cout<<"==========="<<endl;
+        cout<<"Item: "<<node->title<<endl;
+        cout<<"Attribute: +"<<node->attribute<<endl;
+        cout<<"Cost: "<<node->cost<<" gp"<<endl;
+        cout<<"Quantity: "<< node->quantity<<endl;
+        cout<<"==========="<<endl;
+        cout<< endl;
+    }
+    /*
+    if (found==false)
+        cout<<"Item not found."<<endl;
+        */
+}
+///1//////////////////////////////////////////////////////////
+
+
+///Buys item from the shop/////////////////////////////////////
+void MyShop::buyItem(std::string title, int itemAttribute, int itemCost, int itemQuantity)
+{
+    ShopNode* node=root;
+    bool found;
+	while (node!=NULL)
+	{
+		if (node->title.compare(title)>0)
+			node=node->leftChild;
+		else if (node->title.compare(title)<0)
+			node=node->rightChild;
+		else if ((node->title==title) && (node->attribute == itemAttribute) && (node->cost == itemCost))
+        {
+            found=true;
+            break;
+        }
+        else
+        {
+            found=false;
+            break;
+        }
+	}
+
+	if (found==true) //&& node->quantity!=0
+    {
+        cout<< endl;
+        cout<<node->title << " has been sold!"<<endl;
+        node->quantity--;
+        cout<<"==========="<<endl;
+        cout<<"Item info"<<endl;
+        cout<<"==========="<<endl;
+        cout<<"Item: "<<node->title<<endl;
+        cout<<"cost: "<<node->cost<<endl;
+        cout<<"Quantity: "<<node->quantity<<endl;
+        cout<<"==========="<<endl;
+        cout<< endl;
+        addPlayerInventory(title, itemAttribute, itemCost, itemQuantity);
+    }
+    else if (found==true && node->quantity==0)
+    {
+        cout<< endl;
+        cout<< node->title<< " x 0" <<endl;
+        cout<< "Unfortunately We Are Out Of " << node->title <<endl;
+        cout<< endl;
+    }
+    else if (found==false){
+        cout<<"Item not found."<<endl;
+    }
+
+}
+///1////////////////////////////////////////////////////////////////////
+
+
+///Prints out the inventory in the store(Helper)/////////////////////////////////
+void MyShop::printShopInventory()
+{
+    cout<<endl;
+    cout<<"========================"<<endl;
+    cout<<"Items In The Shop"<<endl;
+    cout<<"========================"<<endl;
+    printShopInventory(root);
+    cout<<"========================"<<endl;
+    cout<<endl;
+}
+///1//////////////////////////////////////////////////////////////////
+
+
+///Prints out inventory////////////////////////////////////////////
+void MyShop::printShopInventory(ShopNode* node)
+{
+
+    if (node->leftChild!=NULL){
+        printShopInventory(node->leftChild);
+    }
+    if (node->rightChild!=NULL){
+        printShopInventory(node->rightChild);
+    }
+    cout<< "- " <<node->title<<" x "<<node->quantity<<endl;
+}
+///1/////////////////////////////////////////////////////////
+
+///Print Player Inventory//////////////////////////////////
+void MyShop::printPlayerInventory()
+{
+    cout<<endl;
+    cout<<"========================"<<endl;
+    cout<<"Items In Your Inventory"<<endl;
+    cout<<"========================"<<endl;
+    printPlayerInventory(playerRoot);
+    cout<<"========================"<<endl;
+    cout<<endl;
+}
+///1////////////////////////////////////////////////////////
+
+///Prints the players Inventory!!!/////////////////////////
+void MyShop::printPlayerInventory(PlayerNode* node)
+{
+
+    if (node->leftChild!=NULL){
+        printPlayerInventory(node->leftChild);
+    }
+    if (node->rightChild!=NULL){
+        printPlayerInventory(node->rightChild);
+    }
+    cout<< "- " <<node->itemName<<" x "<<node->itemQuantity<<endl;
+}
+///1///////////////////////////////////////////////////////////
+
+
+
+
+
---
+++ myShop.h
@@ -0,0 +1,83 @@
+#ifndef MYSHOP_H
+#define MYSHOP_H
+#include <iostream>
+
+struct ShopNode{
+    int attribute;
+    std::string title;
+    int cost;
+    int quantity;
+
+    ShopNode *parent;
+    ShopNode *leftChild;
+    ShopNode *rightChild;
+
+    ShopNode(){};
+
+    ShopNode(std::string in_title, int in_attribute, int in_cost, int in_quantity)
+    {
+        title = in_title;
+        quantity = in_quantity;
+        attribute = in_attribute;
+        cost = in_cost;
+        parent = NULL;
+        leftChild = NULL;
+        rightChild = NULL;
+    }
+
+};
+
+struct PlayerNode{
+    int itemAttribute;
+    std::string itemName;
+    int itemCost;
+    int itemQuantity;
+
+    PlayerNode *parent;
+    PlayerNode *leftChild;
+    PlayerNode *rightChild;
+
+    PlayerNode(){};
+
+    PlayerNode(std::string in_title, int in_attribute, int in_cost, int in_quantity)
+    {
+        itemName = in_title;
+        itemQuantity = in_quantity;
+        itemAttribute = in_attribute;
+        itemCost = in_cost;
+        parent = NULL;
+        leftChild = NULL;
+        rightChild = NULL;
+    }
+
+};
+
+class MyShop
+{
+
+    public:
+        MyShop();
+        ~MyShop();
+        void printShopInventory();
+        void printPlayerInventory();
+        void sellItem(std::string itemName);
+        void addMovieNode(std::string title, int attribute, int cost, int quantity);
+        void addPlayerNode(std::string itemName, int itemAttribute, int itemCost, int itemQuantity);
+        void findMovie(std::string title);
+        void buyItem(std::string title, int itemAttribute, int itemCost, int itemQuantity);
+        void addPlayerInventory(std::string itemName, int itemAttribute, int itemCost, int itemQuantity);
+
+
+    protected:
+
+    private:
+        void DeleteAll(ShopNode * node); //use this for the post-order traversal deletion of the tree
+        void printShopInventory(ShopNode* node);
+        void printPlayerInventory(PlayerNode* node);
+        ShopNode* search(std::string title);
+        //MovieNode* treeMinimum(MovieNode *node);
+        ShopNode *root;
+        PlayerNode *playerRoot;
+};
+
+#endif // MOVIETREE_H
